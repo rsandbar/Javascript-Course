dd42ae6eb360cca75c91fcaddaf405b3b8bb7105
rsandbar <rsandbar@gmail.com>
2022-06-26 21:42:00 -0500

    cambios realizados para la actividad 1

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,16 @@
+<<<<<<< HEAD
 console.log("hello world");
 
 
 console.log("laater");
+=======
+let nombApe = 'Richard Sandoval Barrientos';
+let profesion = 'Ingeniero de sistemas';
+let newpuesto = 'Full Stack Developer';
+
+const mensaje = `Hola, me llamo ${nombApe} y soy ${profesion}. 
+Estoy cursando este Máster porque me gustaría trabajar en ${newpuesto}`
+
+export{mensaje};
+//console.log(mensaje);
+>>>>>>> aaabce0 (cambios realizados para la actividad 1)
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,5 +1,11 @@
 // Ejercicio 1
+<<<<<<< HEAD
 import "./aboutme.js";
+=======
+
+import {mensaje} from './aboutme.js';
+console.log(mensaje);
+>>>>>>> aaabce0 (cambios realizados para la actividad 1)
 console.log("Ejercicio 1");
 
 // Ejercicio 2
