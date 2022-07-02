64ea8393c430fd2731423f50e12290c55553f3a0
rsandbar <rsandbar@gmail.com>
2022-07-02 18:16:00 -0500

    Cambios finales para presentación de actividad 1

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -12,9 +12,13 @@ const mensaje = `Hola, me llamo ${nombApe} y soy ${profesion}.
 Estoy cursando este Máster porque me gustaría trabajar en ${newpuesto}`
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 export{mensaje};
 //console.log(mensaje);
 >>>>>>> aaabce0 (cambios realizados para la actividad 1)
 =======
 export{mensaje};
 >>>>>>> 751c537 (Confirmación de cambios)
+=======
+export{mensaje};
+>>>>>>> 7c36769 (Cambios finales para presentación de actividad 1)
