30ffcec461085694c7176c8f039d5c68f21f2e74
rsandbar <rsandbar@gmail.com>
2022-07-02 19:20:50 -0500

    Confirmación de cambios

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -11,6 +11,10 @@ let newpuesto = 'Full Stack Developer';
 const mensaje = `Hola, me llamo ${nombApe} y soy ${profesion}. 
 Estoy cursando este Máster porque me gustaría trabajar en ${newpuesto}`
 
+<<<<<<< HEAD
 export{mensaje};
 //console.log(mensaje);
 >>>>>>> aaabce0 (cambios realizados para la actividad 1)
+=======
+export{mensaje};
+>>>>>>> 751c537 (Confirmación de cambios)
